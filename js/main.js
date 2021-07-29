7e88d89a7f6203f8cbefd11ce82f0af780d5b542
BigFelk <fedferrara89@gmail.com>
2021-07-29 18:59:41 +0200

    fine 2.0

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ function inserire_numeri(){
 var a = rnd5();
 alert(a);
 console.log(a);
-setTimeout(gioco(), 3000);
+setTimeout(gioco, 30000);
 function gioco(){
     var b = inserire_numeri();
     console.log(controllo(a,b));
@@ -42,7 +42,8 @@ function gioco(){
 
 // Un alert() espone 5 numeri generati casualmente.
 // Da li parte un timer di 30 secondi.
-// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto prec
+
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 // Bonus:
 // Introdurre la parte di grafica oltre a quella di logica :occhiolino:
